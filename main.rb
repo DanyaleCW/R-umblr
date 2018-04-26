b0db1d94900edb6ed2f90641f29dd0beee5fc9dd
Danyale <danyalecw@gmail.com>
2018-04-26 14:14:14 -0400

    login route finished

--- main.rb
+++ main.rb
@@ -7,11 +7,13 @@ set :database, {adapter: 'postgresql', database: 'blog'}
 #stores cookies
 enable :sessions
 
+get '/' do
+    erb :index
+end    
+
 get '/signup' do
     erb :'/signup' 
-if sign up successful redirect to profile page
-    can 
-
+#if sign up successful redirect to profile page
 end  
 
 get '/login' do
@@ -22,17 +24,46 @@ get '/profile' do
 # find users login information and redirects to profile    
     @user = User.find(session[:id])
     erb :profile
-end    
+end   
+
+get '/logout' do
+    #clears cookies
+    session[:id] = nil
+    redirect '/login'
+end
+
+get '/profile' do 
+    erb :profile
+end
 
 
 post '/user/login' do
-    @user = User.find_by(email: params[:email], password: parmas[:password],
-    birthday: params[:birthday])
+    @user = User.find_by(email: params[:email], password: params[:password]
+    )
     if @user != nil
-        session[id] = @user.id
-        erb :profile
+        session[:id] = @user.id
+        redirect '/profile'
     else
         #user information not found, redirects to signup page
         redirect '/signup'
     end 
-end               
+end  
+
+post '/user/new' do
+    #creates new user
+    @newuser = User.create(email: params[:email], password: params[:password],
+    birthday: params[:birthday])
+    #sets cookies for user and logs in
+    session[:id] = @newuser.id
+    redirect '/profile'
+    end    
+
+private 
+#Potentially useful function instead of checking if the user exists
+def user_exists?
+    (session[:id] != nil) ? true : false
+end
+
+def current_user
+    User.find(session[:id])
+end    
